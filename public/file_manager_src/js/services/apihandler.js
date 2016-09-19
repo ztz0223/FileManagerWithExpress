acf2b542789b7e16df0eff78aaf340b03156269e
azuo1228 <azuo1228@outlook.com>
2016-09-19 14:28:36 +0800

    CreateFolder using token config

--- public/file_manager_src/js/services/apihandler.js
+++ public/file_manager_src/js/services/apihandler.js
@@ -582,13 +582,20 @@
 
                     self.inprocess = true;
                     self.error = '';
-                    $http.post(url, data).success(function (data, code) {
-                        self.deferredHandler(data, deferred, code);
-                    }).error(function (data, code) {
-                        self.deferredHandler(data, deferred, code, $translate.instant('error_creating_folder'));
-                    })['finally'](function () {
-                        self.inprocess = false;
-                    });
+                    tokenUpdate.getTokenSync().then(
+                        function (token) {
+                            $http.post(url, self.buildTokenConfig(token), data).success(function (data, code) {
+                                self.deferredHandler(data, deferred, code);
+                            }).error(function (data, code) {
+                                self.deferredHandler(data, deferred, code, $translate.instant('error_creating_folder'));
+                            })['finally'](function () {
+                                self.inprocess = false;
+                            });
+                        },
+                        function () {
+                            self.inprocess = false;
+                        }
+                    );
 
                     return deferred.promise;
                 };
